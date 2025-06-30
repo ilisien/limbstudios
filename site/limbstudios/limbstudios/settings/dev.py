0a5a16d31c6bf3d3472de1ab0e8e890b3eb1d1f2
ilisien <ian8lisien@gmail.com>
2025-06-30 09:54:16 -0400

    wagtail setup

---
+++ site/limbstudios/limbstudios/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-9x90(oyplobsn=68q7zf%(b6mb241kv*te55+wcl7x!+3vlusf"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
