6c1bcb887c186e19e3f3a139fd6b1bdc3a259a24
ilisien <ian8lisien@gmail.com>
2025-06-30 14:17:23 -0400

    basic hello world for wagtail

--- limbstudios/home/models.py
+++ limbstudios/home/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 
 from wagtail.models import Page
+from wagtail.fields import RichTextField
 
 
 class HomePage(Page):
-    pass
+    warn_text = RichTextField(blank=True)
+    limbs = models.JSONField(default=list,blank=True)
+
+    content_panels = Page.content_panels + ["warn_text","limbs"]
