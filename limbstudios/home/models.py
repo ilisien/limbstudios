ef5de7d6436608db8ab19ceacfc16ccaa4b5bd50
ilisien <ian8lisien@gmail.com>
2025-07-02 15:36:01 -0400

    wip graphical and backend changes to allow for message text at the top of pages

--- limbstudios/home/models.py
+++ limbstudios/home/models.py
@@ -3,9 +3,17 @@ from django.db import models
 from wagtail.models import Page
 from wagtail.fields import RichTextField
 
-
-class HomePage(Page):
+class PageWMsgs(Page):
+    info_text = RichTextField(blank=True)
     warn_text = RichTextField(blank=True)
+    err_text = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + ["info_text","warn_text","err_text"]
+
+    class Meta:
+        abstract = True
+
+class HomePage(PageWMsgs):
     limbs = models.JSONField(default=list,blank=True)
 
-    content_panels = Page.content_panels + ["warn_text","limbs"]
+    content_panels = PageWMsgs.content_panels + ["limbs"]
